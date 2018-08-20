e4c5047540c448646761d6af22d3c50de114fc78
cwh <cwh_semal@163.com>
2018-08-20 12:19:19 +0800

    添加简单的Exception统一处理类

---
+++ app/src/main/java/com/example/cwh_pc/dailynewsstudy/extension/ExceptionHandle.java
@@ -0,0 +1,137 @@
+package com.example.cwh_pc.dailynewsstudy.extension;
+
+import android.util.Log;
+
+import com.google.gson.JsonParseException;
+import com.google.gson.stream.MalformedJsonException;
+
+import org.json.JSONException;
+
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+
+import retrofit2.HttpException;
+
+
+/**
+ * Created by Administrator on 2017/11/27.
+ */
+
+
+public class ExceptionHandle {
+
+    private static final int UNAUTHORIZED = 401;
+    private static final int FORBIDDEN = 403;
+    private static final int NOT_FOUND = 404;
+    private static final int REQUEST_TIMEOUT = 408;
+    private static final int INTERNAL_SERVER_ERROR = 500;
+    private static final int BAD_GATEWAY = 502;
+    private static final int SERVICE_UNAVAILABLE = 503;
+    private static final int GATEWAY_TIMEOUT = 504;
+
+    public static ResponeThrowable handleException(Throwable e) {
+        ResponeThrowable ex;
+        Log.e("Error tag", "e.toString = " + e.toString() + "\n" + "exception:" + e.getMessage());
+        e.printStackTrace();//打印出堆栈信息，看出错的地方
+        if (e instanceof HttpException) {
+            HttpException httpException = (HttpException) e;
+            ex = new ResponeThrowable(e, ERROR.HTTP_ERROR);
+            switch (httpException.code()) {
+                case UNAUTHORIZED:
+                case FORBIDDEN:
+                case NOT_FOUND:
+                case REQUEST_TIMEOUT:
+                case GATEWAY_TIMEOUT:
+                case INTERNAL_SERVER_ERROR:
+                case BAD_GATEWAY:
+                case SERVICE_UNAVAILABLE:
+                default:
+                    ex.code = httpException.code();
+                    ex.message = "网络错误";
+                    break;
+            }
+            return ex;
+        } else if (e instanceof ServerException) {
+            ServerException resultException = (ServerException) e;
+            ex = new ResponeThrowable(resultException, resultException.code);
+            ex.message = resultException.message;
+            return ex;
+        } else if (e instanceof JsonParseException
+                || e instanceof JSONException ||
+                e instanceof MalformedJsonException
+                /*|| e instanceof ParseException*/) {
+            ex = new ResponeThrowable(e, ERROR.PARSE_ERROR);
+            ex.message = "解析错误";
+            return ex;
+        } else if (e instanceof ConnectException || e instanceof SocketTimeoutException) {
+            ex = new ResponeThrowable(e, ERROR.NETWORD_ERROR);
+            ex.message = "连接失败";
+            return ex;
+        } else if (e instanceof javax.net.ssl.SSLHandshakeException) {
+            ex = new ResponeThrowable(e, ERROR.SSL_ERROR);
+            ex.message = "证书验证失败";
+            return ex;
+        } else if (e instanceof java.net.UnknownHostException) {
+            ex = new ResponeThrowable(e, ERROR.UNKONW_HOST_EXCEPTION);
+            ex.message = "未知HOST";
+            return ex;
+        } else {
+            ex = new ResponeThrowable(e, ERROR.UNKNOWN);
+            ex.message = "未知错误";
+            return ex;
+        }
+    }
+
+
+    /**
+     * 约定异常
+     */
+    public class ERROR {
+        /**
+         * 未知错误
+         */
+        public static final int UNKNOWN = 1000;
+        /**
+         * 解析错误
+         */
+        public static final int PARSE_ERROR = 1001;
+        /**
+         * 网络错误
+         */
+        public static final int NETWORD_ERROR = 1002;
+        /**
+         * 协议出错
+         */
+        public static final int HTTP_ERROR = 1003;
+
+        /**
+         * 证书出错
+         */
+        public static final int SSL_ERROR = 1005;
+        /**
+         * 无网络
+         */
+        public static final int NO_NETWORK = 1006;
+
+        public static final int UNKONW_HOST_EXCEPTION = 1007;
+    }
+
+    public static class ResponeThrowable extends Exception {
+        public int code;
+        public String message;
+
+        public ResponeThrowable(Throwable throwable, int code) {
+            super(throwable);
+            this.code = code;
+        }
+    }
+
+    /**
+     * ServerException发生后，将自动转换为ResponeThrowable 返回在onError(e)中
+     */
+    class ServerException extends RuntimeException {
+        int code;
+        String message;
+    }
+
+}
